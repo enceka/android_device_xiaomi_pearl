afb6e9912393e2a2b9e8259bc0893f33693ebe43
Enceka <enceka@yeah.net>
2024-10-27 13:36:31 +0800

    pearl: overlay: Import power profile from V816.0.3.0.ULHCNXM

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from mt6895-common
 $(call inherit-product, device/xiaomi/mt6895-common/mt6895.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworksResPearl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
